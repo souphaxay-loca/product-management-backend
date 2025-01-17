977c5bd2a01d18f621717202f6867a00aeb5ea57
souphaxay-loca <souphaxay@loca.la>
2025-01-17 18:06:32 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,9 +5,7 @@ require("../../src/models/unit");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
