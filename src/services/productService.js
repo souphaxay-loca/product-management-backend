47d6b99c72ae9d3bf79073e8671accd900e21d1b
souphaxay-loca <souphaxay@loca.la>
2025-01-19 18:41:35 +0700

    update

--- src/services/productService.js
+++ src/services/productService.js
@@ -4,28 +4,33 @@ const productService = {
   findAll: async () => {
     return await Product.find()
       .populate({
-        path: "category_id",
-        foreignField: "category_id", // Match on category_id
-        localField: "category_id", // Match with local category_id
+        path: "category", // The field in the Product schema
+        model: "categories", // Explicitly specify the model
+        match: {}, // Optional, specify additional filter criteria
+        localField: "category", // Local field in Product
+        foreignField: "category_id", // Field in Categories model
       })
       .populate({
-        path: "unit_id",
-        foreignField: "unit_id", // Match on unit_id
-        localField: "unit_id", // Match with local unit_id
+        path: "unit", // The field in the Product schema
+        model: "units", // Explicitly specify the model
+        localField: "unit", // Local field in Product
+        foreignField: "unit_id", // Field in Units model
       });
   },
   findById: async (productID) => {
     const id = parseInt(productID);
     return await Product.findOne({ product_id: id })
       .populate({
-        path: "category_id",
+        path: "category",
+        model: "categories",
+        localField: "category",
         foreignField: "category_id",
-        localField: "category_id",
       })
       .populate({
-        path: "unit_id",
+        path: "unit",
+        model: "units",
+        localField: "unit",
         foreignField: "unit_id",
-        localField: "unit_id",
       });
   },
   create: async (productData) => {
